8edb8ff09f316c5b114d21f04d64058b9da03c40
txtbnj2013@gmail.com <txtbnj2013@gmail.com>
2025-06-29 20:33:43 +0100

    tokenizer 0.0.1

---
+++ tom_text_toolbox/tokenizer.py
@@ -0,0 +1,10 @@
+import nltk
+from nltk.tokenize import word_tokenize
+
+nltk.download("punkt")
+
+def process_caption(caption):
+    ### Filters out non-words and links. Sets them to lowercase to match the dictionary.
+    tokens = word_tokenize(caption)
+    filtered_tokens = [word.lower() for word in tokens if word.isalpha() and "https" not in word]
+    return filtered_tokens
