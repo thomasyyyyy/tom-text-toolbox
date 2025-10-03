beaa08a2b24dc68150af672f857617699ab7957c
txtbnj2013@gmail.com <txtbnj2013@gmail.com>
2025-10-03 18:21:59 +0100

    Added a single period.

--- tom_text_toolbox/tom_text_toolbox_analysis.py
+++ tom_text_toolbox/tom_text_toolbox_analysis.py
@@ -1,8 +1,8 @@
 """Main module."""
 
-from tom_text_toolbox.main import analyse_features
+from tom_text_toolbox import analyse_features
 
-file = "Input file name here"
+file = "text_data_TEST.csv"
 
 def main():
     analyse_features(file = file, liwc = True)
